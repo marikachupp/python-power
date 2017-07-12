c1283ef4c7749bf17fe7b5b46bb54200fb5494ba
Marika Chupp <Marika@Marikas-MacBook-Air.local>
2017-07-12 19:18:51 -0400

    learned variable types and how to assign values to variables, learned how to utilize variables to a small extent

---
+++ business_corgi.py
@@ -0,0 +1,8 @@
+beard = "buy"
+must = "sell"
+print beard + must
+print (beard + must) * 10
+ten=10
+print (beard+must)*ten
+print "menu"
+
---
+++ numberz.py
@@ -0,0 +1,10 @@
+numerator = 100
+denominator = 5
+print "int division:", (numerator / denominator)
+denominator = 6
+print "int quotient", (numerator / denominator)
+print "int remainder", (numerator % denominator)
+denominator=5.0
+print "float quotient", (numerator/denominator)
+denominator=6.0
+print "float quotient", (numerator/denominator)
