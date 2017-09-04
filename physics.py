830d7cbb686ef155e40b9f4dd4fe1554168fb633
marika <marikachupp@gmail.com>
2017-09-04 18:41:19 -0400

    Identified the importance of what you name your variables/functions. functions - verbs, variables- nouns. Changed the names of the kinematic functions so they are as straight forward as possible. ie: velocity -> calc_vel_pos (calculate velocity based on position) and final velocity -> calc_vel_acc (calculate velocity based on acceleration) These names will make it much easier once we start using the functions in different contexts.

--- physics.py
+++ physics.py
@@ -1,32 +1,32 @@
 # delta stands for displacement or change in position
-def velocity(time, final_pos, initial_pos=0):
+def calc_vel_pos(time, final_pos, initial_pos=0):
     delta = final_pos - initial_pos
     return delta / float(time)
 
 
-def final_position(time, vel, initial_pos=0):
+def calc_pos_vel(time, vel, initial_pos=0):
     return vel * time + initial_pos
 
 
-def acceleration(time, final_vel, initial_vel=0):
+def calc_acc_vel(time, final_vel, initial_vel=0):
     delta_vel = final_vel - initial_vel
     return delta_vel / float(time)
 
 
-def final_velocity(time, acceleration, initial_vel):
-    return time * acceleration + initial_vel
+def calc_vel_acc(time, acc, initial_vel):
+    return time * acc + initial_vel
 
 
-print final_velocity(4, 3, 2) == 14.0
-print final_velocity(10, 5, 5) == 55.0
+print calc_vel_pos(10, 300, 200) == 10.0
+print calc_vel_pos(10, 300) == 30.0
+print calc_vel_pos(10, 5, 4) == 0.1
 
-print velocity(10, 300, 200) == 10.0
-print velocity(10, 300) == 30.0
-print velocity(10, 5, 4) == 0.1
+print calc_pos_vel(2, 5, 10) == 20.0
+print calc_pos_vel(4, 13, 12) == 64.0
+print calc_pos_vel(2.5, 3) == 7.5
 
-print final_position(2, 5, 10) == 20.0
-print final_position(4, 13, 12) == 64.0
-print final_position(2.5, 3) == 7.5
+print calc_acc_vel(3, 20, 5) == 5.0
+print calc_acc_vel(100, 50) == 0.5
 
-print acceleration(3, 20, 5) == 5.0
-print acceleration(100, 50) == 0.5
+print calc_vel_acc(4, 3, 2) == 14.0
+print calc_vel_acc(10, 5, 5) == 55.0
