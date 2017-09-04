dea09a39f3090d9256fb72b6cae796935e7e3d66
marika <marikachupp@gmail.com>
2017-09-04 18:10:18 -0400

    defined the pythagoren theorum function as a refresher. creating and assigning values to variables. making sure all of the variables needed for the function are present. "test cases" where we test the function based on the expected output for the given inputs.

--- pythagorean.py
+++ pythagorean.py
@@ -1,21 +1,13 @@
 import math
 
 
-def square(x):
-    return x * x
-
-
-def add(x1,x2):
-    return x1 + x2
-
-
 def pythag(a, b):
-    a_2 = square(a)
-    b_2 = square(b)
+    a_2 = a * a
+    b_2 = b * b
     c_2 = a_2 + b_2
     c = math.sqrt(c_2)
     return c
 
 
-print pythag(3,4)
-print pythag(6,14)
+print pythag(3, 4) == 5.0
+print pythag(6, 8) == 10.0
