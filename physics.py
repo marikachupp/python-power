a439f1314cef2d4cfa885482c14f29f17ddb77e1
marika <marikachupp@gmail.com>
2017-09-04 18:20:53 -0400

    took each kinematic equation and identified the variables needed to complete them. in the case of velocity, two variables needed were final position and initial position. because those two variables are required to find the difference in position, we consolidated the change in position into the single variable: delta. Conducted multiple test cases for all of our equations.

--- physics.py
+++ physics.py
@@ -1,8 +1,32 @@
 # delta stands for displacement or change in position
-def velocity(time, x2, x1=0):
-    delta = x2 - x1
-    return delta / time
+def velocity(time, final_pos, initial_pos=0):
+    delta = final_pos - initial_pos
+    return delta / float(time)
 
+
+def final_position(time, vel, initial_pos=0):
+    return vel * time + initial_pos
+
+
+def acceleration(time, final_vel, initial_vel=0):
+    delta_vel = final_vel - initial_vel
+    return delta_vel / float(time)
+
+
+def final_velocity(time, acceleration, initial_vel):
+    return time * acceleration + initial_vel
+
+
+print final_velocity(4, 3, 2) == 14.0
+print final_velocity(10, 5, 5) == 55.0
 
 print velocity(10, 300, 200) == 10.0
 print velocity(10, 300) == 30.0
+print velocity(10, 5, 4) == 0.1
+
+print final_position(2, 5, 10) == 20.0
+print final_position(4, 13, 12) == 64.0
+print final_position(2.5, 3) == 7.5
+
+print acceleration(3, 20, 5) == 5.0
+print acceleration(100, 50) == 0.5
