e95cc0cbcafa5dc912d1d364a32d961d715e8569
marika <marikachupp@gmail.com>
2017-09-04 20:05:30 -0400

    wrote a function to calculate position based on acceleration, using the functions we previously wrote. Incorporated initial velocity and initial position as optional parameters! it was wicked!

--- physics.py
+++ physics.py
@@ -1,3 +1,8 @@
+def is_close(value_one, value_two):
+    import math
+    return "testing if %1.2f equals %1.2f: %s" % (value_one, value_two, math.fabs(value_one - value_two) <= 0.001)
+
+
 # delta stands for displacement or change in position
 def calc_vel_pos(time, final_pos, initial_pos=0):
     delta = final_pos - initial_pos
@@ -17,16 +22,26 @@ def calc_vel_acc(time, acc, initial_vel):
     return time * acc + initial_vel
 
 
-print calc_vel_pos(10, 300, 200) == 10.0
-print calc_vel_pos(10, 300) == 30.0
-print calc_vel_pos(10, 5, 4) == 0.1
+def calc_pos_acc(time, acc, initial_vel=0, initial_pos=0):
+    vel = calc_vel_acc(time, acc, initial_vel)
+    pos = calc_pos_vel(time, vel, initial_pos)
+    return pos
 
-print calc_pos_vel(2, 5, 10) == 20.0
-print calc_pos_vel(4, 13, 12) == 64.0
-print calc_pos_vel(2.5, 3) == 7.5
 
-print calc_acc_vel(3, 20, 5) == 5.0
-print calc_acc_vel(100, 50) == 0.5
+print is_close(calc_pos_acc(3, -9.8), -88.2)
+print is_close(calc_pos_acc(3, -9.81, -10), -118.29)
+print is_close(calc_pos_acc(3, -9.81, -10, 12), -106.29)
 
-print calc_vel_acc(4, 3, 2) == 14.0
-print calc_vel_acc(10, 5, 5) == 55.0
+print is_close(calc_vel_pos(10, 300, 200), 10.0)
+print is_close(calc_vel_pos(10, 300), 30.0)
+print is_close(calc_vel_pos(10, 5, 4), 0.1)
+
+print is_close(calc_pos_vel(2, 5, 10), 20.0)
+print is_close(calc_pos_vel(4, 13, 12), 64.0)
+print is_close(calc_pos_vel(2.5, 3), 7.5)
+
+print is_close(calc_acc_vel(3, 20, 5), 5.0)
+print is_close(calc_acc_vel(100, 50), 0.5)
+
+print is_close(calc_vel_acc(4, 3, 2), 14.0)
+print is_close(calc_vel_acc(10, 5, 5), 55.0)
