c24344029f9f178bb3e06e647caac152375f9808
marika <marikachupp@gmail.com>
2017-12-24 18:42:43 -0500

    previously in this file we used different kinds of collections to yield the same result. Now we did the same thing but with a class. A class is not a kind of collection, but it is a way to store data. In a list you can change a value by referring to the index, and setting a new value;In a set, you can change values and append a value, but only if it maintains a collection of UNIQUE elements. Tuples are immutable and cannot be changed. Dictionaries, you define a key and a value, like a word and its definition. These all require that you know(to some extent) the order of the values, and the values themselves. When using classes on the other hand, you dont need to know any values, or the order of them.

--- tulips.py
+++ tulips.py
@@ -12,3 +12,23 @@ def fancy_dict(n, d):
 
 print fancy_dict(4, 3) == {'q': 1, 'r': 1}
 print fancy_dict(421, 33) == {'q': 12, 'r': 25}
+
+
+class Complex:
+    def __init__(self, q, r):
+        self.q = q
+        self.r = r
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.q == other.q and self.r == other.r
+
+
+def fancy_complex(n, d):
+    return Complex(n / d, n % d)
+
+
+print fancy_complex(4, 3) == 1.33
+print fancy_complex(4, 3) == Complex(1, 1)
+print fancy_complex(421, 33) == Complex(12, 25)
